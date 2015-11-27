05144b2830d27a319b2619acbee05000aaef750a
huzhengquan <froons@gmail.com>
2015-11-27 10:09:36 +0800

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+var path=require('path');
+
+module.exports = function(){
+  //production
+  //dev
+  const configFile = (process.env.NODE_ENV || "dev")+'.config.json';
+  const readConfig = function(config,dir){
+    try{
+      let newConfig = require(path.join(dir,configFile));
+      return readConfig(Object.assign(newConfig, config),path.join(dir,'../'));
+    }catch(e){
+      return config;
+    }
+  }
+  return readConfig({},path.resolve('./'));
+}
+
---
+++ test/test.js
@@ -0,0 +1,2 @@
+var a=require('../index.js');
+console.log('a',a());
